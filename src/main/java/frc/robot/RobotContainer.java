422f3a7d9f9b2cad1060cb3de8bbdf987bf4b80e
Yunju607 <yj207lee@gmail.com>
2025-02-22 10:28:49 -0600

    Yo We're cooked - updates and debugging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ControllerConstants;
@@ -110,7 +111,10 @@ public class RobotContainer {
                 }))); */
         // Configure the trigger bindings
         configureBindings();
-        
+        /*NamedCommands.registerCommand("Testing", new SequentialCommandGroup(
+                new InstantCommand(() -> {
+
+                }))); */
     }
 
     // Command shootAction =
